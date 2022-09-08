24a0a02e62d2366e635a1fd36cff5a0e2b8cdf95
simocarbo <104845863+simocarbo@users.noreply.github.com>
2022-09-08 11:38:04 +0200

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,13 @@
+//freccina top
+function scrollToTop() {
+           window.scrollTo(0, 0);
+        }
+function showCookie() {
+      let cookie = document.querySelector('#cookie');
+      cookie.style = "display:block;";
+    }
+//cookie
+function hideCookie() {
+      let cookie = document.querySelector('#cookie');
+      cookie.style = "display:none;";
+    }
